5740bd11df4bd7f22551b3fad53ace3183bcbf47
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-10-15 06:04:54 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,22 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Piyumi");
+
+        int x = 5;   //Defining a variable
+        double y = 3.78;
+
+        System.out.println("x = "+x);
+
+//        string and characters
+
+         String depf = "chpter";
+         char node = 'd';
+
+         String count = depf+567; //string with int
+         String grow = node + "custom"; // string with char
+
+        String nff = "example" + count+ "now:" +grow;
+        System.out.println(nff);
+    }
+}
