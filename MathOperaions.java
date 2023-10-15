00d5bb1d2f8400e5bc0dcba96ffdc6784254e644
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-10-15 06:12:34 +0530

    Add files via upload

---
+++ MathOperaions.java
@@ -0,0 +1,12 @@
+public class MathOperaions {
+    public static void main(String[] args) {
+        int x  = 6;
+        double y = 45.6;
+
+        double z = 2*x + 10;
+
+        double w = 4*y+7;
+        System.out.println(z);
+        System.out.println(w);
+    }
+}
