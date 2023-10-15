3ba6598f2b8094e8d153d696e6f4d4258bef0f31
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-10-15 21:37:18 +0530

    Add files via upload

---
+++ StringOperation.java
@@ -0,0 +1,7 @@
+public class StringOperation {
+    public static void main(String[] args) {
+        String fullName = "Bardon" + " "+"Lee"+ " "+ "Kra";
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println(fullNameUpper);
+    }
+}
