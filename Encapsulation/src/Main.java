62c63ec4335f3c78ec9b9609fd45a178aa20307c
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-11-03 10:27:31 +0530

    Add files via upload

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.setId(123);
+        e1.setName("piyumi");
+        System.out.println(e1.getId());
+    }
+}
