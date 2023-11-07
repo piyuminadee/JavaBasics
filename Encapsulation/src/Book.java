54ad9bc5a6f60f8a2e1a0f1ce8018771cf7b77c2
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-11-07 11:58:25 +0530

    Add files via upload

---
+++ Encapsulation/src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    private String bookName;
+    private int price;
+    private String publiserName;
+
+    public String getBookName()
+    {
+        //this.bookName = bookName;
+        return bookName;
+    }
+    public void setBookName(String bookName){
+        this.bookName = bookName;
+    }
+
+
+}
---
+++ Encapsulation/src/BookMain.java
@@ -0,0 +1,7 @@
+public class BookMain {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+       b1.setBookName("Petsemetary");
+        System.out.println("Book Name: "+ b1.getBookName());
+    }
+}
