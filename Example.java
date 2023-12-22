8c7427bcae14d85470a0b1f917791112c090ee98
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2023-12-22 07:53:23 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    final int constatntValue =23;
+
+    final int anotherConstValue;
+
+
+    public Example(int value) {
+        this.anotherConstValue = value;
+    }
+}
