1ee7d2f06752c67e8a8215069e587142c54a45ee
Piyumi Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2024-01-04 18:32:35 +0530

    Add files via upload
    JFrame Foundation

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount(3444.44, "john", 1344);
-        System.out.println("Initial balance: "+ myAccount.getBalance());
+        JFrame jFrame;
+        jFrame = new JFrame();
+        jFrame .setSize(400,300);
+        jFrame.setVisible(true);
+        JOptionPane.showMessageDialog(jFrame, "How \nare you?");
+        //JOptionPane.showMessageDialog(null, "Good Bye");
+        JOptionPane.showInputDialog(null, "Enter Text:");
+        String input;
+
+        //input = JOptionPane.showInputDialog(null, "Enter Text");
+
+        //To input an integer value
+        String str = JOptionPane.showInputDialog(null, "Enter Age");
+        int age = Integer.parseInt(str);
+
 
     }
-
 }
