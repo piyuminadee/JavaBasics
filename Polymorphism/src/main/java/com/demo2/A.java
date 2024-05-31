03637f2f1e7895ba3a972c7bc65dd982c9f124c4
Nadeesh Weerarathna <87165894+piyuminadee@users.noreply.github.com>
2024-05-31 13:07:04 +0530

    behavior of polymorphism
    object in each time change behavior according to their reference class and created class

---
+++ Polymorphism/src/main/java/com/demo2/A.java
@@ -0,0 +1,8 @@
+package com.demo2;
+
+public class A {
+
+    public void show(){
+        System.out.println("in A");
+    }
+}
---
+++ Polymorphism/src/main/java/com/demo2/B.java
@@ -0,0 +1,8 @@
+package com.demo2;
+
+public class B extends A{
+
+    public void show(){
+        System.out.println("In B");
+    }
+}
